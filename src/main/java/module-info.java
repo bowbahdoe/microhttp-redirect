21d884f063ea8a506a212b57139cec22811ed356
Ethan McCue <ethan@mccue.dev>
2024-04-20 12:13:29 -0400

    Add jspecify require

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module dev.mccue.microhttp.redirect {
     requires transitive org.microhttp;
     requires transitive dev.mccue.microhttp.handler;
     requires dev.mccue.reasonphrase;
+    requires static org.jspecify;
 
     exports dev.mccue.microhttp.redirect;
 }
